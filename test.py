405c8e77b7f52b58da9c3a6dfcbc1c177e297bf8
cat-programmer2 <140415791+cat-programmer2@users.noreply.github.com>
2023-07-26 20:50:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
 from io import BytesIO
 import base64
 from PIL import Image
-import logging
 import asyncio
 
 from kandinsky import generate
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     img_bytes, censored = asyncio.run(generate('cat', height=512, width=1024, timeout=-1))
